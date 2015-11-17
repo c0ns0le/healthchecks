2e701a5b4bcbded3121b655a911e286d47d94081
teivarodiere <teiva.rodiere@gmail.com>
2015-11-17 14:17:12 +1000

    asd
    asd

--- vmware/vmwareModules.psm1
+++ vmware/vmwareModules.psm1
@@ -98,11 +98,7 @@ function getIssues(
 						$totalIssues = 0
 						$deviceIndex=1
 						$dataTable = $objArray | sort Name | %{
-							$obj = $_
-							Write-Host "________________________"
-							Write-Host $obj.vCenter
-							Write-Host "________________________"
-							pause
+							$obj = $_							
 							$myvCenter = $global:srvConnection | ?{$_.Name -eq $obj.vCenter}
 							$intervalsSecs = (Get-StatInterval -Server $myvCenter | ?{$_.Name -eq "Past Day"}).SamplingPeriodSecs
 							Write-Progress -Activity "$title" -Id 2 -ParentId 1 -Status "$deviceIndex/$($objArray.Count) :- $($myvCenter.Name)\$($obj.Name)..." -PercentComplete (($deviceIndex/$($objArray.Count))*100)
@@ -865,6 +861,11 @@ function getIssues(
 						$deviceIndex=1
 						$dataTable = $objArray | sort Name | %{
 							$obj = $_
+							Write-Host "________________________"
+							Write-Host $obj.vCenter
+							Write-Host "________________________"
+							pause
+							
 							$myvCenter=$srvConnection | ?{$_.Name -eq $obj.vCenter}
 							$intervalsSecs = (Get-StatInterval -Server $myvCenter | ?{$_.Name -eq "Past Day"}).SamplingPeriodSecs
 							Write-Progress -Activity "$title" -Id 2 -ParentId 1 -Status "$deviceIndex/$($objArray.Count) :- $($myvCenter.Name)\$($obj.Name)..." -PercentComplete (($deviceIndex/$($objArray.Count))*100)
